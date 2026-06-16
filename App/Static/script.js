d4b46a26806861f48f64a5c8351ad264852e9592
anu739303-lang <anu739303@gmail.com>
2026-06-16 12:15:02 +0530

    Fix static paths, set audio src attribute, add debug display, and initialize players for each audio element

--- App/Static/script.js
+++ App/Static/script.js
@@ -9,14 +9,17 @@
             media: function(target) {
                 var media = $('audio.fc-media', (target !== undefined) ? target : 'body');
                 if (media.length) {
-                    media.mediaelementplayer({
-                        audioHeight: 40,
-                    features : ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
-                        alwaysShowControls: true,
-                        timeAndDurationSeparator: '<span></span>',
-                        iPadUseNativeControls: true,
-                        iPhoneUseNativeControls: true,
-                        AndroidUseNativeControls: true
+                    media.each(function(){
+                        var $m = $(this);
+                        $m.mediaelementplayer({
+                            audioHeight: 40,
+                            features: ['playpause', 'current', 'duration', 'progress', 'volume', 'tracks', 'fullscreen'],
+                            alwaysShowControls: true,
+                            timeAndDurationSeparator: '<span></span>',
+                            iPadUseNativeControls: true,
+                            iPhoneUseNativeControls: true,
+                            AndroidUseNativeControls: true
+                        });
                     });
                 }
             },
